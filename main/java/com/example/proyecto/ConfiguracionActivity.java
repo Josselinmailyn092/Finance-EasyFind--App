27e482efe1a38541ce3e4574eb3d6d55da9d9435
Josselinmailyn092 <mailynjoss@gmail.com>
2024-07-08 19:46:03 -0500

    Actualizar variables database

--- main/java/com/example/proyecto/ConfiguracionActivity.java
+++ main/java/com/example/proyecto/ConfiguracionActivity.java
@@ -2,10 +2,10 @@ package com.example.proyecto;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-import androidx.appcompat.app.AppCompatActivity;
 
 public class ConfiguracionActivity extends AppCompatActivity {
 
