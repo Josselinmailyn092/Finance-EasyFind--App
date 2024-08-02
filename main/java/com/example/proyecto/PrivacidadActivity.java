649308928b7a5181a8cce624cc7e5cc8e1f5bf48
Josselinmailyn092 <mailynjoss@gmail.com>
2024-08-02 12:44:53 -0500

    Interfaz Privacidad

--- main/java/com/example/proyecto/PrivacidadActivity.java
+++ main/java/com/example/proyecto/PrivacidadActivity.java
@@ -1,4 +1,27 @@
 package com.example.proyecto;
 
-public class PrivacidadActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PrivacidadActivity extends AppCompatActivity {
+
+    private ImageButton btnClosePrivacidad;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_privacidad);
+
+        btnClosePrivacidad = findViewById(R.id.btn_close_privacidad);
+
+        btnClosePrivacidad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Close the activity
+                finish();
+            }
+        });
+    }
 }
