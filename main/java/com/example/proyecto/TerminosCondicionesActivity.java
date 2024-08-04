f6893f0a8d90496b1b7bf16bc5e36d537ddc2e2b
Josselinmailyn092 <mailynjoss@gmail.com>
2024-08-03 22:28:47 -0500

    actualización xml

--- main/java/com/example/proyecto/TerminosCondicionesActivity.java
+++ main/java/com/example/proyecto/TerminosCondicionesActivity.java
@@ -1,3 +1,19 @@
 package com.example.proyecto;
-public class TerminosCondicionesActivity {
+
+import android.os.Bundle;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TerminosCondicionesActivity extends AppCompatActivity {
+    private ImageButton btnCerrar;
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terminoscondiciones);
+
+        btnCerrar= findViewById(R.id.btnCerrar_terminosC);
+        btnCerrar.setOnClickListener(v -> finish());
+    }
 }
